e43a9018f2ec28f5d19d7d3360a378b6687ac4ec
Prince Sanjivy <34624548+princesanjivy@users.noreply.github.com>
2023-11-16 19:47:07 +0000

    removed header tag from the errorInfo page

--- components/errorInfo.tsx
+++ components/errorInfo.tsx
@@ -1,9 +1,6 @@
-import Header from "./header";
-
 const ErrorInfo = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header description="" />
       <div className="flex flex-grow justify-center items-center text-center">
         <h1 className="text-emerald-500 font-semibold">
           Something went wrong, please scan the QR code again!
